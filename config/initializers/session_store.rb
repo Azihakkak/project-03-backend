15f65d84421cc603b76b7c0d81821aaa7165288e
sam nejat <samnejat@sams-mbp.home>
2020-04-29 23:50:57 +1000

    fixed user authentication

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_GlamB', domain: 'https://localhost:3000'
+  Rails.application.config.session_store :cookie_store, key: '_GlamB', domain: 'https://glam-b.herokuapp.com/'
 else
   Rails.application.config.session_store :cookie_store, key: '_GlamB'
 end
